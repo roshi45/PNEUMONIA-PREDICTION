3dc393c66443f4e63cc5f1a3427284e608626151
ROSHINI PRIYA <125353722+roshi45@users.noreply.github.com>
2024-11-29 16:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def index():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
